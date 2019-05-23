29cd577c9d6782893193a350f0934e34c2131217
wearenothing <39990563+wearenothing@users.noreply.github.com>
2019-05-23 15:49:48 +0800

    Update polynomial.cpp

--- polynomial.cpp
+++ polynomial.cpp
@@ -9,14 +9,14 @@ struct PNode
 	struct PNode* next;
 };
 
-void Attach(int coef, int expon, Poly rear)
+void Attach(int coef, int expon, Poly* rear)
 {
 	Poly S = (Poly)malloc(sizeof(struct PNode));
 	S->coef = coef;
 	S->expon = expon;
 	S->next = NULL;
-	rear->next = S;
-	rear = S;
+	(*rear)->next = S;
+	*rear = S;
 }
 
 Poly AddPoly(Poly P1, Poly P2)
@@ -32,37 +32,19 @@ Poly AddPoly(Poly P1, Poly P2)
 		{
 			int sum = P1->coef + P2->coef;
 			if (sum)
-			{
-				Poly S= (Poly)malloc(sizeof(struct PNode));
-				S->coef = sum;
-				S->expon = P1->expon;
-				S->next = NULL;
-				rear->next = S;
-				rear = S;
-			}
+			Attach(sum, P1->expon, &rear);
 			P1 = P1->next;
 			P2 = P2->next;
 		}
 		else if (P1->expon > P2->expon)
 		{
-			Poly S = (Poly)malloc(sizeof(struct PNode));
-			S->coef = P1->coef;
-			S->expon = P1->expon;
-			S->next = NULL;
-			rear->next = S;
-			rear = S;
+			Attach(P1->coef, P1->expon, &rear);
 			P1 = P1->next;
 		}
 		else
 		{
-			Poly S = (Poly)malloc(sizeof(struct PNode));
-			S->coef = P2->coef;
-			S->expon = P2->expon;
-			S->next = NULL;
-			rear->next = S;
-			rear = S;
+			Attach(P2->coef, P2->expon, &rear);
 			P2 = P2->next;
-			
 		}
 	}
 	if (P1)
@@ -78,40 +60,57 @@ Poly AddPoly(Poly P1, Poly P2)
 	free(t);
 	return front;
 }
-//¡„∂‡œÓ Ω
-
+//Èõ∂Â§öÈ°πÂºè
+void PrintPoly(Poly P)
+{
+	//int count = 0;
+	bool tag = false;
+	if (P)
+	{
+		Poly t = P;
+		while (t)
+		{
+			if (!tag)
+			{
+				cout << t->coef << " " << t->expon;
+				tag = true;
+			}
+			else
+				cout <<" "<< t->coef << " " << t->expon;
+			t = t->next;
+		}
+	}
+	else
+		cout << "0 0" << endl;
+}
 
 int main()
 {
 	int n1,n2;
 	cin >> n1;
-	PNode P1;//change PNode to Poly ,try? or try Poly Node=(Poly)malloc(sizeof(struct PNode)); watch P
-	P1.next = NULL;
-	Poly rear1 = &P1;
+	Poly P1 = (Poly)malloc(sizeof(struct PNode));
+	P1->next = NULL;
+	Poly rear1 = P1; 
 	for (int i = 0; i < n1; i++)
 	{
-		Poly S = (Poly)malloc(sizeof(struct PNode));
-		cin >> S->coef >> S->expon;
-		S->next = NULL;
-		rear1->next = S;
-		rear1 = S;
+		int coef, expon;
+		cin >> coef >> expon;
+		Attach(coef, expon, &rear1);
 	}
 
 	cin >> n2;
-	Poly P2= (Poly)malloc(sizeof(struct PNode));//change PNode to Poly ,try? or try Poly Node=(Poly)malloc(sizeof(struct PNode)); watch P
+	Poly P2= (Poly)malloc(sizeof(struct PNode));
 	P2->next = NULL;
 	Poly rear2 = P2;
 	for (int i = 0; i < n2; i++)
 	{
-		Poly S = (Poly)malloc(sizeof(struct PNode));
-		cin >> S->coef >> S->expon;
-		S->next = NULL;
-		rear2->next = S;
-		rear2 = S;
+		int coef, expon;
+		cin >> coef >> expon;
+		Attach(coef, expon, &rear2);
 	}
-	Poly t1 = P1.next;
-	Poly t2 = P2->next;
-	Poly P = AddPoly(t1, t2);
-
+	P1 = P1->next;
+	P2 = P2->next;
+	Poly P = AddPoly(P1, P2);
+	PrintPoly(P);
 	return 0;
 }
